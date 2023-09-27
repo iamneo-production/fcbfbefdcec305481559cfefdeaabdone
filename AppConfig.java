ff4d0e5ddde6420297e639fe9855eae87488db6c
Iamneo <techadmin@iamneo.ai>
2023-09-27 11:41:44 +0000

    Backup Commit

---
+++ AppConfig.java
@@ -0,0 +1,50 @@
+public class Student {
+
+        private int stdentId;
+            private String firstName;
+                private String lastName;
+
+                    public Student() {
+                        }
+
+                            public Student(int stdentId, String firstName, String lastName) {
+                                    this.stdentId = stdentId;
+                                            this.firstName = firstName;
+                                                    this.lastName = lastName;
+                                                        }
+
+                                                            public int getStdentId() {
+                                                                    return stdentId;
+                                                                        }
+
+                                                                            public void setStdentId(int stdentId) {
+                                                                                    this.stdentId = stdentId;
+                                                                                        }
+
+                                                                                            public String getFirstName() {
+                                                                                                    return firstName;
+                                                                                                        }
+
+                                                                                                            public void setFirstName(String firstName) {
+                                                                                                                    this.firstName = firstName;
+                                                                                                                        }
+
+                                                                                                                            public String getLastName() {
+                                                                                                                                    return lastName;
+                                                                                                                                        }
+
+                                                                                                                                            public void setLastName(String lastName) {
+                                                                                                                                                    this.lastName = lastName;
+                                                                                                                                                        }
+
+                                                                                                                                                            @Override
+                                                                                                                                                                public String toString() {
+                                                                                                                                                                        return "Student{" +
+                                                                                                                                                                                        "stdentId=" + stdentId +
+                                                                                                                                                                                                        ", firstName='" + firstName + '\'' +
+                                                                                                                                                                                                                        ", lastName='" + lastName + '\'' +
+                                                                                                                                                                                                                                        '}';
+                                                                                                                                                                                                                                            }
+                                                                                                                                                                                                                                            }
+                                                                                                                                                                                                                                            
+}
