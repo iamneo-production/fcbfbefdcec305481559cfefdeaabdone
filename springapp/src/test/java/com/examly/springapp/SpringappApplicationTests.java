ce94977e48db46465501dd7253c79709ff99b05d
Iamneo <techadmin@iamneo.ai>
2023-10-09 09:58:48 +0000

    Backup Commit

--- springapp/src/test/java/com/examly/springapp/SpringappApplicationTests.java
+++ springapp/src/test/java/com/examly/springapp/SpringappApplicationTests.java
@@ -3,15 +3,15 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class SpringappApplicationTests {
     public static void main(String[] args) {
-            // Load the Spring XML configuration file
-                    ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        // Load the Spring XML configuration file
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-                            // Retrieve the Student bean from the container
-                                    Student student = (Student) context.getBean("student");
+        // Retrieve the Student bean from the container
+        Student student = (Student) context.getBean("student");
 
-                                            // Access and print student details
-                                                    System.out.println("Student ID: " + student.getStudentId());
-                                                            System.out.println("First Name: " + student.getFirstName());
-                                                                    System.out.println("Last Name: " + student.getLastName());
-                                                                        }
-                                                                        }
+        // Access and print student details
+        System.out.println("Student ID: " + student.getStudentId());
+        System.out.println("First Name: " + student.getFirstName());
+        System.out.println("Last Name: " + student.getLastName());
+    }
+}
