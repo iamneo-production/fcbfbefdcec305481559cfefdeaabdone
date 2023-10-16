8140535794a0d27d44ca7be490eeba65cfcd1478
Iamneo <techadmin@iamneo.ai>
2023-10-16 15:04:08 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/configuration/AppConfig.java
+++ springapp/src/main/java/com/examly/springapp/configuration/AppConfig.java
@@ -2,15 +2,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.examly.springapp.model.*;
+
 @Configuration
 public class AppConfig {
     @Bean
-        public Student student() {
-                Student student = new Student();
-                        student.setStudentId(1);
-                                student.setFirstName("John");
-                                        student.setLastName("Doe");
-                                                return student;
-                                                    }
-                                                    }
-                                                    
+    public Student student() {
+        Student student = new Student();
+        student.setStudentId(1);
+        student.setFirstName("John");
+        student.setLastName("Doe");
+        return student;
+    }
+}
--- springapp/src/main/java/com/examly/springapp/model/Student.java
+++ springapp/src/main/java/com/examly/springapp/model/Student.java
@@ -1,31 +1,32 @@
 package com.examly.springapp.model;
+
 public class Student {
-        private int studentId;
-            private String firstName;
-                private String lastName;
+    private int studentId;
+    private String firstName;
+    private String lastName;
 
-                    // Getters and setters for the fields
-                        public int getStudentId() {
-                                return studentId;
-                                    }
+    // Getters and setters for the fields
+    public int getStudentId() {
+        return studentId;
+    }
 
-                                        public void setStudentId(int studentId) {
-                                                this.studentId = studentId;
-                                                    }
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
 
-                                                        public String getFirstName() {
-                                                                return firstName;
-                                                                    }
+    public String getFirstName() {
+        return firstName;
+    }
 
-                                                                        public void setFirstName(String firstName) {
-                                                                                this.firstName = firstName;
-                                                                                    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
-                                                                                        public String getLastName() {
-                                                                                                return lastName;
-                                                                                                    }
+    public String getLastName() {
+        return lastName;
+    }
 
-                                                                                                        public void setLastName(String lastName) {
-                                                                                                                this.lastName = lastName;
-                                                                                                                    }
-                                                                                                                    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
